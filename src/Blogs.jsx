e11917304aa9e4d9c3a70913c3cc656a24ab98f4
Islam AbdelRadi <81393667+islamabdelradi1@users.noreply.github.com>
2022-10-12 01:05:23 +0200

    Update Blogs.jsx

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -21,7 +21,7 @@ function Blogs() {
     const [products,setproducts]=useState([]);
 
 useEffect(()=>{
-    axios.get('http://localhost:9000/products/')
+    axios.get('https://fashion-beta.vercel.app/products/')
     .then( (res)=> {
       // handle success
       setproducts(res.data);
